a833eb233506c7ff37ce56191113af510725200b
Alexander Ermakov <barbalion@gmail.com>
2021-03-16 19:23:33 +0300

    Updated according to new tool available on MS site.

--- win10_download_iso.cmd
+++ win10_download_iso.cmd
@@ -1,8 +1,9 @@
 @echo off
-set _TOOLPATH=MediaCreationTool2004.exe
+set _TOOLPATH=MediaCreationTool20H2.exe
 set _EDITION=Enterprise
 set _LOCALE=en-US
 
+cd /d "%~dp0"
 %_TOOLPATH% /Eula Accept /Retail /MediaLangCode %_LOCALE% /MediaArch x64 /MediaEdition %_EDITION%
 if errorlevel 1 (
   echo %_TOOLPATH% is not found. Please download it and start the script again. Prees any key to exit, the download link will open.
