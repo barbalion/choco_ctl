1035ce3edae9661dd0076dc2f98ac8f1f6ba31cb
Alexander Ermakov <barbalion@gmail.com>
2021-04-05 10:40:39 +0300

    More general support of MediaCreationTool.

--- win10_download_iso.cmd
+++ win10_download_iso.cmd
@@ -1,12 +1,15 @@
-@echo off
-set _TOOLPATH=MediaCreationTool20H2.exe
+rem @echo off
 set _EDITION=Enterprise
 set _LOCALE=en-US
+set _ARCH=x64
 
-cd /d "%~dp0"
-%_TOOLPATH% /Eula Accept /Retail /MediaLangCode %_LOCALE% /MediaArch x64 /MediaEdition %_EDITION%
-if errorlevel 1 (
-  echo %_TOOLPATH% is not found. Please download it and start the script again. Prees any key to exit, the download link will open.
+for %%f in ("%~dp0MediaCreationTool*.exe") do set _TOOLPATH=MediaCreationTool20H2.exe
+if not defined _TOOLPATH (
+  echo MediaCreationTool*.exe is not found. Please download it and start the script again. Prees any key to exit, the download link will open.
   pause
   start https://go.microsoft.com/fwlink/?LinkId=691209
+  exit /b 1
 )
+
+cd /d "%~dp0"
+start %_TOOLPATH% /Eula Accept /Retail /MediaLangCode %_LOCALE% /MediaArch %_ARCH% /MediaEdition %_EDITION%
